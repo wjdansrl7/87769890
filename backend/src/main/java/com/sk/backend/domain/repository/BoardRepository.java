d0d3e53ce29dbabe43a22a7d4308ae3f4bbea794
wjdansrl7 <c880910@naver.com>
2024-12-01 14:55:06 +0900

    Feat: 유저, 게시판 레포지토리 생성

---
+++ backend/src/main/java/com/sk/backend/domain/repository/BoardRepository.java
@@ -0,0 +1,14 @@
+package com.sk.backend.domain.repository;
+
+import com.sk.backend.domain.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * packageName    : com.sk.backend.domain.repository
+ * fileName       : BoardRepository
+ * author         : moongi
+ * date           : 12/1/24
+ * description    :
+ */
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
---
+++ backend/src/main/java/com/sk/backend/domain/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.sk.backend.domain.repository;
+
+import com.sk.backend.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * packageName    : com.sk.backend.domain.repository
+ * fileName       : UserRepository
+ * author         : moongi
+ * date           : 12/1/24
+ * description    :
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+}
