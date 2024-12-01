c1d97aac76de8e16793dce9033b42a826394b16a
wjdansrl7 <c880910@naver.com>
2024-12-01 14:39:17 +0900

    Feat: backend project 초기 세팅

---
+++ backend/src/main/java/com/sk/backend/BackendApplication.java
@@ -0,0 +1,16 @@
+package com.sk.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
